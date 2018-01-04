4b3d69c23b074c41672100b22c19ea75b7a8efc9
Flamy <27062841+f1amy@users.noreply.github.com>
2018-01-05 01:49:35 +0500

    Add project files

---
+++ Resources/JS/cookie.js
@@ -0,0 +1,45 @@
+// return cookie with name 'name', if it exist, if not, then return undefined
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// set cookie with name 'name' and value 'value'
+// options - object with properties cookie (expires, path, domain, secure)
+function setCookie(name, value, options) {
+    options = options || {};
+
+    var expires = options.expires;
+
+    if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    var updatedCookie = name + "=" + value;
+
+    for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+            updatedCookie += "=" + propValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+// deletes cookie with name 'name'
+function deleteCookie(name) {
+    setCookie(name, "", {
+        expires: -1
+    })
+}
