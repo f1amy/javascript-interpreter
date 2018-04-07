e89d4285301118cbbd7ec7b3599fcfe3988bd3f2
Flamy <27062841+f1amy@users.noreply.github.com>
2018-04-08 01:11:28 +0500

    switched from the use of cookies to local storage and other improvements

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,148 +1,144 @@
-document.addEventListener('DOMContentLoaded', function () {
-    RestoreJSCodeFromCookie();
+"use strict";
 
-    window.addEventListener("beforeunload", SaveJSCodeToCookie);
+document.addEventListener("DOMContentLoaded", function() {
+  restoreJSCode();
 
-    document.getElementById('save-code').addEventListener('click', SaveJSCodeToCookie);
+  window.onbeforeunload = storeJSCode;
 
-    document.getElementById('restore-code').addEventListener('click', RestoreJSCodeFromCookie);
+  document.getElementById("save-code").onclick = storeJSCode;
 
-    document.getElementById('clear-right-area').addEventListener('click', ClearRightArea);
+  document.getElementById("restore-code").onclick = restoreJSCode;
 
-    document.getElementById('left-textarea').addEventListener('keydown', function (event) {
-        if (event.keyCode != 9) {
-            return;
+  document.getElementById("clear-right-area").onclick = clearRightArea;
+
+  document.getElementById("left-textarea").onkeydown = function(event) {
+    if (event.keyCode !== 9) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const object = this;
+    const start = object.selectionStart;
+    const end = object.selectionEnd;
+    const before = object.value.substring(0, start);
+    const after = object.value.substring(end, object.value.length);
+
+    object.value = before + "\t" + after;
+    object.setSelectionRange(start + 1, start + 1);
+  };
+
+  document.getElementById("run-code").onclick = function() {
+    const leftTextArea = document.getElementById("left-textarea");
+    let rightTextArea = document.getElementById("right-textarea");
+
+    if (leftTextArea.value !== "") {
+      try {
+        clearRightArea();
+
+        let timeSpent = performance.now();
+        const result = eval(leftTextArea.value);
+
+        timeSpent = performance.now() - timeSpent;
+
+        document.getElementById("completion-time").innerHTML =
+          timeSpent.toFixed(3) + "ms";
+
+        if (result !== undefined) {
+          rightTextArea.value += result;
         }
+      } catch (Exception) {
+        rightTextArea.value += `Uncaught ${Exception.name}: ${
+          Exception.message
+        }.`;
+      }
+    }
+  };
 
-        event.preventDefault();
+  document.getElementById("clear-left-area").onclick = function() {
+    const answer = !!confirm(
+      "Do you really want to clear the text from the " +
+        "left text area?\nAll your code will be lost!"
+    );
 
-        let obj = this;
-        let start = obj.selectionStart;
-        let end = obj.selectionEnd;
-        let before = obj.value.substring(0, start);
-        let after = obj.value.substring(end, obj.value.length);
+    if (answer === true) {
+      document.getElementById("left-textarea").value = "";
+    }
+  };
 
-        obj.value = before + '\t' + after;
-        obj.setSelectionRange(start + 1, start + 1);
-    });
+  document.getElementById("download-code").onclick = function() {
+    const leftTextArea = document.getElementById("left-textarea");
 
-    document.getElementById('run-code').addEventListener('click', function () {
-        let leftTextArea = document.getElementById('left-textarea');
-        let rightTextArea = document.getElementById('right-textarea');
+    if (leftTextArea.value !== "") {
+      const fileContent = leftTextArea.value;
+      const fileName = "Your JavaScript code.js";
+      let fileLink = document.createElement("a");
 
-        if (leftTextArea.value != '') {
-            try {
-                ClearRightArea();
+      fileLink.href = URL.createObjectURL(
+        new Blob([fileContent], {
+          type: "text/javascript"
+        })
+      );
 
-                let timeSpent = performance.now();
-                let result = eval(leftTextArea.value);
-
-                timeSpent = performance.now() - timeSpent;
-
-                document.getElementById('completion-time').innerHTML = timeSpent.toFixed(3) + 'ms';
-
-                if (result != undefined) {
-                    rightTextArea.value += result;
-                }
-            } catch (error) {
-                rightTextArea.value += `Uncaught ${error.name}: ${error.message}.`;
-            }
-        }
-    });
-
-    document.getElementById('clear-left-area').addEventListener('click', function () {
-        let answer = confirm('Do you really want to clear the text from the left text area?\nAll your code will be lost!');
-        let leftTextArea = document.getElementById('left-textarea');
-
-        if (leftTextArea.value != '') {
-            if (answer == true) {
-                leftTextArea.value = '';
-            }
-        }
-    });
-
-    document.getElementById('download-code').addEventListener('click', function () {
-        let leftTextArea = document.getElementById('left-textarea');
-
-        if (leftTextArea.value != '') {
-            let fileContent = leftTextArea.value;
-            let fileName = 'JavaScript code.js';
-            let fileLink = document.createElement("a");
-
-            fileLink.href = URL.createObjectURL(new Blob([fileContent], {
-                type: 'text/javascript'
-            }));
-
-            fileLink.download = fileName;
-            document.body.appendChild(fileLink);
-            fileLink.click();
-            document.body.removeChild(fileLink);
-        }
-    });
+      fileLink.download = fileName;
+      // document.body.appendChild(fileLink);
+      fileLink.click(); // (?) up down comment
+      // document.body.removeChild(fileLink);
+    }
+  };
 });
 
 /**
- * Function saves JS code to cookies from the
- * left text area if it's already don't.
+ * Stores JS code from the left text area if it's already don't.
  */
-function SaveJSCodeToCookie() {
-    let code = document.getElementById('left-textarea').value;
+function storeJSCode() {
+  const code = document.getElementById("left-textarea").value;
 
-    if (code != getCookie('jscode')) {
-        let date = new Date();
-        date.setDate(date.getDate() + 365);
-        let options = {
-            expires: date.toUTCString()
-        }
+  if (code !== localStorage.getItem("jscode")) {
+    localStorage.setItem("jscode", code);
 
-        setCookie('jscode', code, options);
-        console.log('Code stored successfully.');
-    } else {
-        console.log('Code already stored.');
-    }
+    console.log("Code stored successfully.");
+  } else {
+    console.log("Code already stored.");
+  }
 }
 
 /**
- * Function restores JS code from cookies and places 
- * it into left text area if it's not undefined and empty.
+ * Restores JS code from local storage and places
+ * it into the left text area if it's not undefined and empty.
  */
-function RestoreJSCodeFromCookie() {
-    let code = getCookie('jscode');
+function restoreJSCode() {
+  const code = localStorage.getItem("jscode");
 
-    if (code != undefined && code != '') {
-        document.getElementById('left-textarea').value = code;
-        console.log('Code restored successfully.');
-    } else {
-        console.log('No code found for restore.');
-    }
+  if (code !== undefined && code !== "") {
+    document.getElementById("left-textarea").value = code;
+
+    console.log("Code restored successfully.");
+  } else {
+    console.log("No code found for restore.");
+  }
 }
 
 /**
- * Function writes given text to the right text area.
- * 
- * @param {any} text 
+ * Writes given text to the right text area.
+ * @param {string} text Input text.
  */
 function write(text) {
-    document.getElementById('right-textarea').value += `${text}`;
+  document.getElementById("right-textarea").value += text;
 }
 
 /**
- * Function writes given text to the right text area
+ * Writes given text to the right text area
  * and after that places the '\n' symbol same way.
- * @param {any} text 
+ * @param {string} text Input text.
  */
 function writeln(text) {
-    document.getElementById('right-textarea').value += `${text}\n`;
+  document.getElementById("right-textarea").value += `${text}\n`;
 }
 
 /**
- * Function clears all the text in the right text area if it's not empty.
- * 
+ * Clears all text in the right text area, if it's not already empty.
  */
-function ClearRightArea() {
-    let rightTextArea = document.getElementById('right-textarea');
-
-    if (rightTextArea.value != '') {
-        rightTextArea.value = '';
-    }
+function clearRightArea() {
+  document.getElementById("right-textarea").value = "";
 }
