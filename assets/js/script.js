fc5f91150b2ca0a2bf80959a34cf032890a0e0a7
Flamy <27062841+f1amy@users.noreply.github.com>
2018-05-07 20:18:46 +0500

    code style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 "use strict";
 
 function storeJSCode() {
-    localStorage.setItem(
-        "jsCode",
-        encodeURIComponent(document.getElementById("left-textarea").value)
-    );
+  localStorage.setItem(
+    "jsCode",
+    encodeURIComponent(document.getElementById("left-textarea").value)
+  );
 }
 
 function restoreJSCode() {
-    document.getElementById("left-textarea").value = decodeURIComponent(
-        localStorage.getItem("jsCode") || ""
-    );
+  document.getElementById("left-textarea").value = decodeURIComponent(
+    localStorage.getItem("jsCode") || ""
+  );
 }
 
 function write(text) {
-    document.getElementById("right-textarea").value += text;
+  document.getElementById("right-textarea").value += text;
 }
 
 function writeln(text) {
-    document.getElementById("right-textarea").value += `${text}\n`;
+  document.getElementById("right-textarea").value += `${text}\n`;
 }
 
 function clearRightArea() {
-    document.getElementById("right-textarea").value = "";
+  document.getElementById("right-textarea").value = "";
 }
 
 restoreJSCode();
@@ -32,61 +32,61 @@ document.getElementById("restore-code").onclick = restoreJSCode;
 document.getElementById("clear-right-area").onclick = clearRightArea;
 
 document.getElementById("left-textarea").onkeydown = function(event) {
-    if (event.keyCode !== 9) {
-        return;
-    }
+  if (event.keyCode !== 9) {
+    return;
+  }
 
-    event.preventDefault();
+  event.preventDefault();
 
-    const start = this.selectionStart;
+  const start = this.selectionStart;
 
-    this.value =
-        this.value.substring(0, start) +
-        "\t" +
-        this.value.substring(this.selectionEnd, this.value.length);
-    this.setSelectionRange(start + 1, start + 1);
+  this.value =
+    this.value.substring(0, start) +
+    "\t" +
+    this.value.substring(this.selectionEnd, this.value.length);
+  this.setSelectionRange(start + 1, start + 1);
 };
 
 document.getElementById("run-code").onclick = function() {
-    clearRightArea();
+  clearRightArea();
 
-    let returnedValue;
-    let timeSpent = performance.now();
+  let returnedValue;
+  let timeSpent = performance.now();
 
-    try {
-        returnedValue = eval(document.getElementById("left-textarea").value);
-    } catch (exception) {
-        returnedValue = `Uncaught ${exception.name}: ${exception.message}.`;
-    }
+  try {
+    returnedValue = eval(document.getElementById("left-textarea").value);
+  } catch (exception) {
+    returnedValue = `Uncaught ${exception.name}: ${exception.message}.`;
+  }
 
-    timeSpent = performance.now() - timeSpent;
-    document.getElementById("completion-time").innerHTML =
-        timeSpent.toFixed(3) + "ms";
-    document.getElementById("right-textarea").value += returnedValue;
+  timeSpent = performance.now() - timeSpent;
+  document.getElementById("completion-time").innerHTML =
+    timeSpent.toFixed(3) + "ms";
+  document.getElementById("right-textarea").value += returnedValue;
 };
 
 document.getElementById("clear-left-area").onclick = function() {
-    const answer = !!confirm(
-        "Do you really want to clear the text from the " +
-            "left text area?\nAll your code will be lost!"
-    );
+  const answer = !!confirm(
+    "Do you really want to clear the text from the " +
+      "left text area?\nAll your code will be lost!"
+  );
 
-    if (answer) {
-        document.getElementById("left-textarea").value = "";
-    }
+  if (answer) {
+    document.getElementById("left-textarea").value = "";
+  }
 };
 
 document.getElementById("download-code").onclick = function() {
-    let fileLink = window.document.createElement("a");
+  let fileLink = window.document.createElement("a");
 
-    fileLink.href = window.URL.createObjectURL(
-        new Blob([document.getElementById("left-textarea").value], {
-            type: "text/javascript"
-        })
-    );
+  fileLink.href = window.URL.createObjectURL(
+    new Blob([document.getElementById("left-textarea").value], {
+      type: "text/javascript"
+    })
+  );
 
-    fileLink.download = "JavaScript code.js";
-    document.body.appendChild(fileLink);
-    fileLink.click();
-    document.body.removeChild(fileLink);
+  fileLink.download = "JavaScript code.js";
+  document.body.appendChild(fileLink);
+  fileLink.click();
+  document.body.removeChild(fileLink);
 };
